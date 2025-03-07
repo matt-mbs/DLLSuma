48463594f8e67e5f2db63d56abb3640e3ef8b413
Usuario <Usuario@B6-603-37794>
2025-03-07 13:45:14 -0500

    Agregar archivos de proyecto.

---
+++ DLLsuma/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    namespace DLLSuma
+    {
+        public class Class1
+        {
+            public static long Suma(long a, long b)
+            {
+                return a + b;
+            }
+
+        }
+    }
+
